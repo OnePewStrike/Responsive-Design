875dbfdb38ec46181359ca59092178b3f3a02546
Godwin Duliente <rualgodwin@gmail.com>
2023-02-10 16:08:13 +0800

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+// Navbar Module
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-xmark');
+  navbar.classList.toggle('open');
+}
